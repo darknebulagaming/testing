7571552ed4d6390574d5698725e2515d8e7c6bc4
Spencer <spencer@ddsytems.net>
2017-08-31 12:44:27 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MyNewComponentComponent } from './my-new-component/my-new-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyNewComponentComponent
   ],
   imports: [
     BrowserModule,
